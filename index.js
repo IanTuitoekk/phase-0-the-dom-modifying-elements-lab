262fc5f134ca2d5f71639f703fc8d3bfc62c73dd
IanTuitoekk <iantuitoek4@gmail.com>
2025-06-30 15:28:00 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const el = document.querySelector("main#main")
+el.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+
+newHeader.textContent = 'Ian is the champion'
